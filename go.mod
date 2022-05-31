563c53745c3fa0361a7de7e4055133f28a3ad80d
Gaurang Shukla <99.22.shukla.gaurang@gmail.com>
2022-05-31 09:44:58 +0530

    sdvdsv

---
+++ go.mod
@@ -0,0 +1,13 @@
+module auth
+
+go 1.16
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-gonic/gin v1.7.2
+	github.com/go-playground/validator/v10 v10.6.1
+	github.com/heroku/x v0.0.31
+	github.com/joho/godotenv v1.3.0
+	go.mongodb.org/mongo-driver v1.5.3
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e
+)
---
+++ middleware/authMiddleware.go
@@ -0,0 +1,42 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+
+	helper "auth/helpers"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Authz validates token and authorizes users
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("No Authorization header provided")})
+			c.Abort()
+			return
+		}
+
+		claims, err := helper.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("first_name", claims.First_name)
+		c.Set("last_name", claims.Last_name)
+		c.Set("uid", claims.Uid)
+		c.Set("user_type", claims.User_type)
+
+		c.Next()
+
+	}
+}
